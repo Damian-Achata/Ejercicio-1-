a7a109614b308d40b3250bb979d26df7120d8277
Rhaast <89270996+Damian-Achata@users.noreply.github.com>
2022-09-06 22:21:24 -0300

    Add files via upload

---
+++ tp1_backend.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ejercicio</h1>
+
+    <?php 
+        $saludo = "Hello World";
+        echo $saludo;
+
+        $numero = "30";
+
+        $numero2 ="25";
+        
+        $string = "suma";
+        
+        echo $string;
+
+        echo $numero + $numero2;
+
+        echo $string = "Resta";
+
+        echo $numero - $numero2;
+
+        echo $string = "multiplicacion";
+
+        echo $numero * $numero2;
+
+        echo $string = "Division entera";
+
+        echo $numero / $numero2;
+
+        echo $string = "Resto division";
+
+        echo $numero % $numero2;
+
+        echo $string = "Convertir grados celsius a farenheit";
+
+        echo $numero=20 * 1.8 + 32;
+
+        //Calcular area rectangulo//
+
+        $base = "18";
+
+        $altura = "12";
+        
+        $area;
+
+        echo $string = "Area del rectangulo";
+
+        echo $area  = $base * $altura;
+
+        //calcular perimetro rectangulo//
+
+        $perimetro;
+
+        echo $string = "Perimetro del rectangulo";
+        
+        echo $perimetro = $base * 2 + $altura * 2;
+
+        //calcular area triangulo//
+
+        echo $string = "Area del circulo";
+
+        $radio = "30";
+
+        echo $area = 3.14 * 30 ** 2;
+
+        //calcular perimetro//
+
+        echo $string= "Perimetro del circulo";
+        
+
+        echo $perimetro = 2 * 3.14 * 30;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+        
+    
+    ?>
+    
+</body>
+</html>
